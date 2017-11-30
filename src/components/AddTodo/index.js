bc5f53223980a761e3b9beca80eb08807fa9b893
wsavd <tuve@rootfest.net>
2017-11-30 14:10:22 +0300

    value={this.state.text} отправится на сервер

--- src/components/AddTodo/index.js
+++ src/components/AddTodo/index.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-const addTodo = () => {
-  return (
-    <div>
-      <input type="text" />
-    </div>
-  )
+class addTodo extends Component{
+  
+  state = {
+    text: ''
+  }
+
+  handleTextChange = (event) => {
+    this.setState({
+      text: event.target.value
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <input type='text' placeholder='Что добавить?' value={this.state.text} onChange={this.handleTextChange} />
+      </div>
+    )
+  }
 }
 
 export default addTodo
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { render } from 'react-dom';
 import Hello from './Hello';
 import AddTodo from './components/AddTodo'
+import TodoList from './components/TodoList'
 
 const App = () => (
   <div>
     <AddTodo />
+    <TodoList />
   </div>
 );
 
